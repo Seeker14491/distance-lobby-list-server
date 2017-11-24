a0ef27cdf66031484af96d73a6d91e372f1d7884
Brian Bowman <seeker14491@gmail.com>
2017-11-23 22:37:24 -0600

    Comply with CORS

--- Assets/SimpleWebServer.cs
+++ Assets/SimpleWebServer.cs
@@ -44,6 +44,7 @@ public class WebServer {
                             string rstr = _responderMethod(ctx.Request);
                             byte[] buf = Encoding.UTF8.GetBytes(rstr);
                             ctx.Response.ContentLength64 = buf.Length;
+                            ctx.Response.AddHeader("Access-Control-Allow-Origin", "*");
                             ctx.Response.OutputStream.Write(buf, 0, buf.Length);
                         }
                         catch { } // suppress any exceptions
